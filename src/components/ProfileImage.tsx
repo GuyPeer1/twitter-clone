b35844846bdf81474f838cae78d74a6a019ee7a0
Guy Peer <peer.g2@gmail.com>
2023-07-03 17:40:12 +0300

    support infinite scrolling, better sidebar

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -1,14 +1,22 @@
 import Image from "next/image"
+import { VscAccount } from "react-icons/vsc"
 
 type ProfileImageProps = {
-    src?: String | null
-    className?: String
+    src?: string | null
+    className?: string
 
 }
 
 export function ProfileImage({ src, className = "" }: ProfileImageProps) {
-    return <div className={`relative h-12 w-12 overflow-hidden rounded-full ${className}`}>
-        {src == null ? null : <Image src={src} alt="Profile Image" quality={100} fill />}
-
-    </div>
-}
+    return (
+      <div
+        className={`relative h-12 w-12 overflow-hidden rounded-full ${className}`}
+      >
+        {src == null ? (
+          <VscAccount className="h-full w-full" />
+        ) : (
+          <Image src={src} alt="Profile Image" quality={100} fill />
+        )}
+      </div>
+    )
+  }
