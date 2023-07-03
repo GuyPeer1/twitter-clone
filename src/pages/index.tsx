65b3f1b3829a37ddb691da4d8f7fe1a13ca429db
Guy Peer <peer.g2@gmail.com>
2023-07-03 15:30:29 +0300

    added tweets feed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
-import {type NextPage} from "next"
+import { type NextPage } from "next"
+import { InfiniteTweetList } from "~/components/InfiniteTweetList"
 import { NewTweetForm } from "~/components/NewTweetForm"
+import { api } from "~/utils/api"
 
 const Home: NextPage = (props) => {
   return <>
-  <header className="sticky top-0 z-10 border-b bg-white pt-2">
-    <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
-  </header>
-  <NewTweetForm />
+    <header className="sticky top-0 z-10 border-b bg-white pt-2">
+      <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
+    </header>
+    <NewTweetForm />
+    <RecentTweets />
   </>
 }
-export default  Home
+
+function RecentTweets() {
+  const tweets = api.tweet.infinteFeed.useInfiniteQuery({}, { getNextPageParam: (lastPage) => lastPage.nextCursor })
+  return <InfiniteTweetList tweets={tweets.data?.pages.flatMap((page) =>
+    page.twetts)}
+    isError={tweets.isError}
+    isLoading={tweets.isLoading}
+    hasMore={tweets.hasNextPage}
+    fetchNewTweets={tweets.fetchNextPage}
+  />
+}
+export default Home
--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -1,11 +1,58 @@
-import { z } from "zod";
+import { Prisma } from "@prisma/client"
+import { contextProps } from "@trpc/react-query/shared"
+import { z } from "zod"
 import {
   createTRPCRouter,
   publicProcedure,
   protectedProcedure,
-} from "~/server/api/trpc";
+} from "~/server/api/trpc"
 
 export const tweetRouter = createTRPCRouter({
+  infinteFeed: publicProcedure.input(
+    z.object({
+      limit: z.number().optional(),
+      cursor: z.object({ id: z.string(), createdAt: z.date() }).
+        optional(),
+    })
+  ).query(async ({ input: { limit = 10, cursor }, ctx }) => {
+    const currentUserId = ctx.session?.user.id
+
+    const data = await ctx.prisma.tweet.findMany({
+      take: limit + 1,
+      cursor: cursor ? { createdAt_id: cursor } : undefined,
+      orderBy: [{ createdAt: "desc" }, { id: "desc" }],
+      select: {
+        id: true,
+        content: true,
+        createdAt: true,
+        _count: { select: { likes: true } },
+        likes: currentUserId == null ? false : { where: { userId: currentUserId } },
+        user: {
+          select: { name: true, id: true, image: true }
+        }
+      }
+    })
+
+    let nextCursor: typeof cursor | undefined
+    if (data.length > limit) {
+      const nextItem = data.pop()
+      if (nextItem != null) {
+        nextCursor = { id: nextItem.id, createdAt: nextItem.createdAt }
+      }
+    }
+    return {
+      twetts: data.map(tweet => {
+        return {
+          id: tweet.id,
+          content: tweet.content,
+          creadtedAt: tweet.createdAt,
+          likeCount: tweet._count.likes,
+          user: { ...tweet.user },
+          likedByme: tweet.likes?.length > 0,
+        }
+      }), nextCursor
+    }
+  }),
   create: protectedProcedure
     .input(z.object({ content: z.string() }))
     .mutation(async ({ input: { content }, ctx }) => {
